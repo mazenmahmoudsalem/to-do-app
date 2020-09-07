c9b7ef2b5060b121527b5da1ea365f5424239677
mazenmahmoudsalem <mazensalem1@outlook.com>
2020-09-07 23:45:35 +0200

    make the style
    1- make the style
    2- make the model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class task(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True, auto_now_add=True)
