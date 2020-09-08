b434863f271bc8b0c2448b5475adca42ae6ffd16
mazenmahmoudsalem <mazensalem1@outlook.com>
2020-09-08 18:23:36 +0200

    make the done task
    1- upgrde the models.py
    2- make the mazen user
    3- make the migrations
    4- add the done system

--- tasks/models.py
+++ tasks/models.py
@@ -3,4 +3,7 @@ from django.db import models
 
 class task(models.Model):
     title = models.CharField(max_length=50)
-    date = models.DateField(auto_now=True, auto_now_add=True)
+    date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from . import models
 
 
 def home(request):
-    context = {}
+    tasks = models.task.objects.all()
+    if request.GET.get("one"):
+        task = models.task.objects.get(title=request.GET.get("one")[5:])
+        task.delete()
+    context = {"tasks": tasks}
     return render(request, "index.html", context)
