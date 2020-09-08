c85621b9cc1b7dd7db5612df59df026d3c051938
mazenmahmoudsalem <mazensalem1@outlook.com>
2020-09-08 21:07:47 +0200

    ending the simple version of the app

--- tasks/models.py
+++ tasks/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class task(models.Model):
     title = models.CharField(max_length=50)
-    date = models.DateField(auto_now=True)
+    date = models.IntegerField()
+    is_ended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- tasks/views.py
+++ tasks/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render, redirect
 from . import models, forms
+import datetime
 
 
 def home(request):
-    tasks = models.task.objects.all()
-    if request.GET.get("one"):
-        task = models.task.objects.get(title=request.GET.get("one")[5:])
+    if request.GET.get("done"):
+        task = models.task.objects.get(title=request.GET.get("done")[5:])
+        task.is_ended = True
+        task.save()
+    if request.GET.get("undone"):
+        task = models.task.objects.get(title=request.GET.get("undone")[7:])
+        task.is_ended = False
+        task.save()
+    if request.GET.get("delete"):
+        task = models.task.objects.get(title=request.GET.get("delete")[7:])
         task.delete()
+    tasks = models.task.objects.all()
+    for task in tasks:
+        if int(task.date) != int(datetime.datetime.now().strftime("%w")):
+            if task.is_ended == True:
+                task.delete()
+    tasks = models.task.objects.all()
     context = {"tasks": tasks}
     return render(request, "index.html", context)
 
 
 def add_task(request):
-    form = forms.add_task_form()
     if request.method == "POST":
         form = forms.add_task_form(request.POST)
         if form.is_valid():
-            form.save()
-        return redirect("tasks:home")
+            nform = form.save(commit=False)
+            nform.date = int(datetime.datetime.now().strftime("%w"))
+            nform.save()
+            return redirect("tasks:home")
+    form = forms.add_task_form()
     context = {"form": form}
     return render(request, "add_task.html", context)
